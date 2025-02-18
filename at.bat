25475579a178c3b22415c2d92fa24d0ae19f658b
NoAngel <155915830+NopAngel@users.noreply.github.com>
2025-02-17 21:14:42 -0400

    task manager: modify (new version 0.0.2)

--- at.bat
+++ at.bat
@@ -1,36 +1,63 @@
 @echo off
-title Administrador de Tareas Personalizado
+title Custom Task Manager
+color 0A
+
+if "%1"=="" goto usage
+if "%2"=="" goto usage
+
+:inicio
+cls
+echo -------------------------------------
+echo        Custom Task Manager
+echo -------------------------------------
+echo.
+echo Use the arrow keys to navigate and Enter to select
+echo.
+
+call :menu
+
+exit /b
 
 :menu
+echo [1] View all tasks
+echo [2] Exit
+echo.
+
+set /p choice=Select an option: 
+
+if "%choice%"=="1" goto view_tasks
+if "%choice%"=="2" goto end
+goto menu
+
+:view_tasks
 cls
-echo -------------------------------------
-echo        Administrador de Tareas
-echo -------------------------------------
-echo.
-echo Selecciona una opción:
-echo.
-echo 1. Ver todas las tareas
-echo 2. Salir
-echo.
-
-set /p choice=Opción:
-
-if "%choice%"=="1" goto ver_tareas
-if "%choice%"=="2" goto fin
-
-:ver_tareas
-cls
-color 0A
+color 0B
 echo ----------------------------
-echo Tareas en Ejecución
+echo Running Tasks
 echo ----------------------------
 echo.
 tasklist
 pause
-goto menu
+goto inicio
 
-:fin
+:end
 cls
-echo Saliendo del Administrador de Tareas...
+color 0C
+echo Exiting Task Manager...
 timeout /t 2 >nul
 exit
+
+:usage
+cls
+echo -------------------------------------
+echo        Custom Task Manager
+echo -------------------------------------
+echo.
+echo Usage: 
+echo %0 [param1] [param2]
+echo.
+echo Parameters:
+echo param1 - Description for param1
+echo param2 - Description for param2
+echo.
+exit /b
